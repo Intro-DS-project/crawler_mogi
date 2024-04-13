9c7f6ea0cd3ab1fdcb67c09c9c2444f9bf330190
hieu748159263 <hieu748159263@gmail.com>
2024-04-13 22:57:16 +0700

    Use gemini api to parse description

--- crawler_mogi/spiders/mogi.py
+++ crawler_mogi/spiders/mogi.py
@@ -1,23 +1,26 @@
 import scrapy
 from scrapy.exceptions import CloseSpider
+from datetime import datetime
 
 from crawler_mogi.items import RoomItem
-from crawler_mogi.utils import address_conversion, price_conversion, date_conversion
+from crawler_mogi.utils import price_convert
+from crawler_mogi.gemini import extract_description, extract_location
 
 
 class MogiSpider(scrapy.Spider):
     name = "mogi"
+    stop = False
 
     def start_requests(self):
         i = 1
         while True:
             yield scrapy.Request(url=f"https://mogi.vn/ha-noi/thue-phong-tro-nha-tro?cp={i}", callback=self.parse)
+            if self.stop:
+                raise CloseSpider("Completed scraping the current day's item.")
             i += 1
 
     def parse(self, response):
         room_items = response.css(".props .link-overlay")
-        if not room_items:
-            raise CloseSpider("No more room items to scrape")
         for item in room_items:
             room_href = item.css("::attr(href)").get()
             yield response.follow(room_href, callback=self.parse_room_detail)
@@ -25,37 +28,37 @@ class MogiSpider(scrapy.Spider):
     def parse_room_detail(self, response):
         item = RoomItem()
 
-        # Địa chỉ: Võ Chí Công, Phường Nghĩa Đô, Quận Cầu Giấy, Hà Nội
-        address = response.css(".address::text").get()
-        (item["street"], item["ward"], item["district"]) = address_conversion(address)
+        # Ngày đăng: 05/04/2024
+        post_date_str = response.css("div.info-attr:nth-child(3) > span:nth-child(2)::text").get()
+        post_date_datetime = datetime.strptime(post_date_str, "%d/%m/%Y").date()
+        current_date = datetime.now().date()
+        if (post_date_datetime < current_date):
+            self.stop = True
+            return
+        item["post_date"] = post_date_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-        # Giá: 3 triệu 800 nghìn
+        # Giá
         price_str = response.css("div.price:nth-child(3)::text").get()
-        item["price"] = price_conversion(price_str)
+        item["price"] = price_convert(price_str)
 
         # Diện tích:  20 m2
         area_str = response.css("div.info-attr:nth-child(1) > span:nth-child(2)::text").get()
         item["area"] = float(area_str.split()[0])
 
-        # Ngày đăng: 05/04/2024
-        post_date_str = response.css("div.info-attr:nth-child(3) > span:nth-child(2)::text").get()
-        item["post_date"] = date_conversion(post_date_str)
+        # Địa chỉ
+        address = response.css(".address::text").get()
+        (item["street"], item["ward"], item["district"], *_) = extract_location(address).split(',')
 
-        # Mô tả
+        # Dùng mô tả điền các trường còn lại
         desc_str = response.css(".info-content-body::text").getall()
-        item["description"] = ' '.join(desc_str)
-
-        # Link
-        item["url"] = response.url
-
-        # Điền các trường còn thiếu
-        item["num_bedroom"] = 0
-        item["num_diningroom"] = 0
-        item["num_kitchen"] = 0
-        item["num_toilet"] = 0
-        item["num_floor"] = 0
-        item["current_floor"] = 0
-        item["direction"] = ""
-        item["street_width"] = 0
+        fields = extract_description(' '.join(desc_str)).split(',')
+        fields_int = []
+        for field in fields:
+            try:
+                fields_int.append(int(field))
+            except ValueError:
+                fields_int.append(field)
+        (item["num_bedroom"], item["num_diningroom"], item["num_kitchen"], item["num_toilet"], item["num_floor"],
+         item["current_floor"], item["direction"], item["street_width"], *_) = fields_int
 
         yield item
--- crawler_mogi/utils.py
+++ crawler_mogi/utils.py
@@ -1,37 +1,15 @@
-from datetime import datetime
-
-def address_conversion(address_str):
-
-    def clean(str, pat):
-        res = str
-        for p in pat:
-            if p in str:
-                res = str.split(p)[-1]
-                break
-        return res.lstrip('0123456789/ ')
-
-    address_parts = address_str.split(",")
-    street = clean(address_parts[-4], ["Phố", "Đường", "phố", "đường", "Ngõ", "ngõ", "Ngách", "ngách"])
-    ward = clean(address_parts[-3], ["Phường", "Xã", "phường", "xã",])
-    district = clean(address_parts[-2], ["Quận", "Huyện", "quận", "huyện"])
-
-    return street, ward, district
-
-
-def price_conversion(price_str):
+def price_convert(price_str):
     # price_in_str = "3 triệu 800 nghìn" -> 3.8
-    # price_in_str = "3 triệu" -> 3
+    # price_in_str = "3 triệu" -> 3.0
     # price_in_str = "700.000 đ" -> 0.7
     price_split = price_str.split()
-    millions = float(price_split[0].replace('.', ''))
-    try:
-        thousands = int(price_split[2])
-    except IndexError:
-        thousands = 0
+
+    if price_split[1] == "đ":
+        millions, thousands = 0, float(price_split[0].replace('.', '')) / 1000
+    else:
+        try:
+            millions, thousands = int(price_split[0]), int(price_split[2])
+        except IndexError:
+            millions, thousands = int(price_split[0]), 0
+
     return millions + thousands / 1000
-
-
-def date_conversion(date_str):
-    date = datetime.strptime(date_str, "%d/%m/%Y")
-    converted_date = date.strftime("%Y-%m-%d %H:%M:%S")
-    return converted_date
